32f0b6fa38b8f61eab5c682e3abcc0c120a9dd67
YangLiu0327 <liuyangyangyang1992@gmail.com>
2024-03-05 22:07:00 +1100

    complete profile page ui

--- api/modals/user.model.js
+++ api/modals/user.model.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema(
         },
         avatar: {
             type: String,
-            default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+            default: "http://up.qqmmgg.com/up/33/97/2c/33972c269fad442b494ee6a6d0fe7a94.jpg"
         },
     },
     {timestamps: true}
