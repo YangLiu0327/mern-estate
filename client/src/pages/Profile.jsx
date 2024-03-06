8c2fe5e0c8abbf3d0e3d773faa046e6409679710
YangLiu0327 <liuyangyangyang1992@gmail.com>
2024-03-06 22:53:21 +1100

    comoplete listing page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { useRef, useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { app } from "../firebase";
 import {
   uploadBytesResumable,
@@ -183,6 +184,12 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "update"}
         </button>
+        <Link
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          to={"/create-listing"}
+        >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
