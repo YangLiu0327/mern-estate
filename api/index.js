a46cdd423eb3d5664ea7c01c11ffb820c23c55d1
YangLiu0327 <liuyangyangyang1992@gmail.com>
2024-03-06 17:20:32 +1100

    create user and update api

--- api/index.js
+++ api/index.js
@@ -8,10 +8,7 @@ import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
-const app = express();
-app.use(express.json());
-
+import cookieParser from 'cookie-parser'; // get information from cookie
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -21,6 +18,10 @@ mongoose.connect(process.env.MONGO)
 .catch((err) => {
     console.log(err)
 });
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000')
