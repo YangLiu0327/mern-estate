6e416e973866d27b8d73283a10695de408a8b4ba
YangLiu0327 <liuyangyangyang1992@gmail.com>
2024-03-07 23:50:02 +1100

    create slider

---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useSelector } from "react-redux";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+import {
+  FaBath,
+  FaBed,
+  FaChair,
+  FaMapMarkedAlt,
+  FaMapMarkerAlt,
+  FaParking,
+  FaShare,
+} from "react-icons/fa";
+
+export default function Listing() {
+  const { currentUser } = useSelector((state) => state.user);
+  SwiperCore.use(Navigation);
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(`/api/listing/get/${params.listingId}`);
+        const data = await res.json();
+        setListing(data);
+        if (data.success === false) {
+          setError(true);
+          setLoading(false);
+          return;
+        }
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchListing();
+  }, [params.listingId]);
+  console.log(listing, "listing");
+  return (
+    <main>
+      {loading && <p className="text-center my-7 text-2xl">Loading ...</p>}
+      {error && (
+        <p className="text-center my-7 text-2xl">Something went wrong</p>
+      )}
+      {listing && !loading && !error && (
+        <Swiper navigation>
+          {listing.imageUrls.map((url) => (
+            <SwiperSlide key={url}>
+              <div
+                className="w-[100%] h-[200px]"
+                style={{
+                  background: `url(${url}) center no-repeat`,
+                  backgroundSize: "contain",
+                }}
+              ></div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </main>
+  );
+}
