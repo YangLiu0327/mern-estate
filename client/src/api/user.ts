812a876f2a813bef6ffc168c43d706ab972c4a0b
YangLiu0327 <liuyangyangyang1992@gmail.com>
2024-03-13 22:43:31 +1100

    added sign up

--- client/src/api/user.ts
+++ client/src/api/user.ts
@@ -1,1 +1,21 @@
-import { AxiosPromise } from 'axios';
+const commonObj = {
+    url: 'http://localhost:3000'
+}
+
+interface requestType {
+    method?: string;
+    url: string;
+    baseURL?: string;
+    headers?: object;
+    data?: object;
+  }
+export const userSignUp = (data): requestType => ({
+    ...commonObj,
+    method: 'POST',
+    url: "/api/auth/signup",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    data,
+})
+
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import useHttp from "../api/useHttp";
+import { userSignUp } from "../api/user";
 
 export default function SignUp() {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+
+  const { sendRequest, loading, error } = useHttp();
   const handleChange = (e) => {
     setFormData({
       ...formData,
@@ -19,27 +21,30 @@ export default function SignUp() {
     // will refresh page
     e.preventDefault();
     try {
-      setLoading(true);
-      const res = await fetch("/api/auth/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      const data = await res.json();
-      console.log(data);
-      if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
-        return;
-      }
-      setLoading(false);
-      setError(null);
+      // setLoading(true);
+      // const res = await fetch("/api/auth/signup", {
+      //   method: "POST",
+      //   headers: {
+      //     "Content-Type": "application/json",
+      //   },
+      //   body: JSON.stringify(formData),
+      // });
+      // const data = await res.json();
+      // console.log(data);
+      // if (data.success === false) {
+      //   setLoading(false);
+      //   setError(data.message);
+      //   return;
+      // }
+      // setLoading(false);
+      // setError(null);
+      const response = await sendRequest(userSignUp(formData));
+      console.log(response, "response");
       navigate("/sign-in");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      // setLoading(false);
+      // setError(error.message);
+      console.log(error);
     }
   };
 
